f707bc507615706eadb19bd61d64633b52c76f88
Kazunori Osaki <claudioropez.k@gmail.com>
2025-05-26 20:44:22 +0000

    Terraformワークフローの環境設定を更新
    - dev環境の指定を'環境: dev'に変更
    - prod環境の指定を'環境: prod'に変更

--- terraform/modules/service_accounts/main.tf
+++ terraform/modules/service_accounts/main.tf
@@ -1,5 +1,6 @@
 resource "google_service_account" "this" {
   for_each     = toset(var.sa_names)
   account_id   = "run-${each.key}-${var.env}"
-  display_name = "${each.key} Cloud Run SA (${var.env})"
+  display_name = "${each.key} Service Account (${var.env})"
+  project      = var.project_id
 }
